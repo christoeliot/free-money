86e742b46d6c078f709420d982c9310c01df44bd
Christo Eliot <christoeliot@gmail.com>
2017-05-17 11:37:24 -0700

    linear optimization ---> genetic algorithm

--- Baseball/lineup_optimizer_TEST.R
+++ Baseball/lineup_optimizer_TEST.R
@@ -1,4 +1,4 @@
-library(lpSolve)
+library(genalg)
 library(stattleshipR)
 library(dplyr)
 library(xlsx)
@@ -81,109 +81,53 @@ stdevs[is.na(stdevs$sd_DK), names(stdevs) %in% 'sd_DK'] <- 0
 summ <- merge(averages, stdevs, by = 'player_id')   
 contest <- merge(contest, summ, by = 'player_id')
 
-# generate lineups matrix or whatever 
+ind_1b <- which(contest$Position %in% c('1B', '1B/2B', '1B/3B', '1B/OF')) 
+ind_2b <- which(contest$Position %in% c('2B', '1B/2B', '2B/OF', '2B/SS', '2B/3B')) 
+ind_3b <- which(contest$Position %in% c('3B','1B/3B', '2B/3B', '3B/SS', '3B/OF'))
+ind_ss <- which(contest$Position %in% c('SS', '1B/SS', '2B/SS', '3B/SS', 'OF/SS'))
+ind_c <- which(contest$Position %in% c('C', '1B/C', '2B/C', '3B/C', 'C/OF'))
+ind_of <- which(contest$Position %in% c('OF', '1B/OF', '2B/OF', '3B/OF', 'C/OF'))
+ind_p <- which(contest$Position %in% c('SP','RP'))
 
-# Prepare constraint matrix of zeros
-A <- matrix(0, nrow = 8, ncol = nrow(contest))
+#update eval function to check for total number of players and types of players
 
-#Set 1B parameters
-j<-1
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='1B'    || 
-      contest$Position[i]=='1B/2B' || 
-      contest$Position[i]=='1B/3B' ||
-      contest$Position[i]=='1B/OF')
-    A[j,i]<-1
-}
-#2B
-j<-2
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='2B'    || 
-      contest$Position[i]=='1B/2B' || 
-      contest$Position[i]== '2B/OF'||
-      contest$Position[i]=='2B/SS' ||
-      contest$Position[i]=='2B/3B')
-    A[j,i]<-1
-}
-#3B
-j<-3
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='3B'    || 
-      contest$Position[i]=='1B/3B' || 
-      contest$Position[i]== '2B/3B'||
-      contest$Position[i]=='3B/SS' ||
-      contest$Position[i]=='3B/OF')
-    A[j,i]<-1
-}
-#SS
-j<-4
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='SS'    || 
-      contest$Position[i]=='1B/SS' || 
-      contest$Position[i]== '2B/SS'||
-      contest$Position[i]=='3B/SS' ||
-      contest$Position[i]=='OF/SS')
-    A[j,i]<-1
-}
-#C
-j<-5
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='C'    || 
-      contest$Position[i]=='1B/C' || 
-      contest$Position[i]== '2B/C'||
-      contest$Position[i]=='3B/C' ||
-      contest$Position[i]=='C/OF')
-    A[j,i]<-1
-}
-#SP
-j<-6
-for (i in 1:nrow(contest)){
-  if (contest$Position[i] %in% c('SP','RP')) 
-    A[j,i]<-1
+evalFunc <- function(x) {
+  team_ind <- which(x == 1)
+  solution_goal <- sum(contest[team_ind, 6])
+  solution_cost <- sum(contest[team_ind, 4])
+  
+  if(solution_cost > salary_cap) return(abs(50000 - solution_cost) / solution_goal)
+  if(sum(x) != 10)  return(200 * (abs(sum(x) - 10)) / solution_goal)
+  if(sum(x[ind_p]) > 2) return(sum(x[ind_p]) * 100 / solution_goal)
+  if(sum(x[ind_1b]) > 1) return(sum(x[ind_1b]) * 50 / solution_goal)
+  if(sum(x[ind_2b]) > 1) return(sum(x[ind_2b]) * 50 / solution_goal)
+  if(sum(x[ind_3b]) > 1) return(sum(x[ind_3b]) * 50 / solution_goal)
+  if(sum(x[ind_ss]) > 1) return(sum(x[ind_ss]) * 50 / solution_goal)
+  if(sum(x[ind_c]) > 1) return(sum(x[ind_c]) * 50 / solution_goal)
+  if(sum(x[ind_of]) > 3) return(sum(x[ind_of]) * 150 / solution_goal)
+  if(sum(x[ind_p]) > 2) return(sum(x[ind_p]) * 50 / solution_goal)
+  return(-solution_goal)
 }
 
-#OF
-j<-7
-for (i in 1:nrow(contest)){
-  if (contest$Position[i]=='OF'    || 
-      contest$Position[i]=='1B/OF' || 
-      contest$Position[i]== '2B/OF'||
-      contest$Position[i]=='3B/OF' ||
-      contest$Position[i]=='C/OF')
-    A[j,i]<-1
-}
-i<-1
 
-A[8, ] <- contest$Salary                # salary <= 50000
 
-# Prepare input for LP solver
-contest <- contest %>% mutate(rand_points = rnorm(n(), mean = avg_DK, sd = sd_DK))
+#Model building
+iter = 500
+player_size <- length(contest[,1])
+#Salary Limit from DK
+salary_cap <- 50000
 
-require(lpSolve)
+GAmodel <- rbga.bin(size = player_size, popSize = 500, iters = iter, mutationChance = 0.01,  evalFunc = evalFunc)
 
-objective.in <- contest$avg_DK
-const.mat <- A
-const.dir <- c('==', '==', '==', '==','==','==','==', '<=')
-const.rhs <- c(1, 1, 1, 1,1,2,3, 50000)
-sol <- lp(direction = 'max', objective.in, const.mat, const.dir, const.rhs, all.bin = TRUE)   
-solution <- contest[which(sol$solution == 1), names(contest) %in% 
-                  c('DK_player_name', 'Position', 'Salary', 
-                    'GameInfo', 'team_abbrev.x', 'AvgPointsPerGame',
-                    'avg_DK', 'sd_DK', 'player_id')]
-solution$number <- with(solution, paste0('lineup_',1))
+cat(genalg:::summary.rbga(GAmodel,echo=T))
 
-# Generate optimal lineup with lp solve
-for(i in 2:25){
-  objective.in <- contest$rand_points
-  sol <- lp(direction = 'max', objective.in, const.mat, const.dir, const.rhs, all.bin = TRUE)   
-  temp <- contest[which(sol$solution == 1), names(contest) %in% 
-                        c('DK_player_name', 'Position', 'Salary', 
-                          'GameInfo', 'team_abbrev.x', 'AvgPointsPerGame',
-                          'avg_DK', 'sd_DK', 'player_id')]
-  temp$number <- with(solution, paste0('lineup_',i))
-  solution <- rbind(solution, temp)
-  contest <- contest %>% mutate(rand_points = rnorm(n(), mean = avg_DK, sd = sd_DK))
-}
+best_solution <- GAmodel$population[which.min(GAmodel$evaluations), ]
+best_ind <- which(best_solution == 1)
+
+contest[best_ind,]
+
+
+
 
 # do 10000 simulations of these lineups (still neeed to add that hitters cannot score negative)
 monte <- matrix(0, nrow = dim(solution)[1], ncol = 11)
@@ -197,11 +141,6 @@ colnames(monte)[1] <- 'lineup_num'
 monte <- lapply(monte, function(x) type.convert(as.character((x))))
 monte_agg <- aggregate(.~lineup_num, monte, sum)
 
-
-
-
-
-##############################################################################################
 ################################## PICKLIST TEST GROUND ######################################
 
 # are there new players in this contest? (maybe add team_abbrev as merge condition?)
@@ -224,55 +163,9 @@ m <- createSheet(wb, sheetName = 'import')
 addDataFrame(missing, m)
 saveWorkbook(wb, 'Baseball/mlb_dk_match_key.xlsx')
 
-#######################################################################################
-
-###################################################################################
-########################## INJURIES ######################################
-
-
-#####################################################################################
-
-
-
-#####################################################################################
-########### GET A BUNCH OF TABLES AT VARYING TIME INTERVALS #########################
-
-# blend into aggregate?
-# get a bunch of lineups
-# monte carlo
-# group by scoring ranges
-# mass enter lineups with strategic variations 
-
-
-######################################################################################
-
-# first pass - just optimize based on FFPG, then iterate what to optimize on
+####################################################################################################################
 
 ### HERE IS THE DUDE WHO FIGURED OUT HOW TO FRAME THE PROBLEM FIRST, LET US BEAT THAT
 ## https://github.com/MattBrown88/lpsolve---Daily-Fantasy-Sports-Optimization/blob/master/LPSolve%20-%20DFS%20Lineup%20Optimization.R
 
-#####################################################################################################
-# CREATE SOLUTIONS LIST OR WHATEVER FOR MONTE CARLO SIM
-#Print players in optimal lineup
 
-# LIST OF 'OPTIMAL LINEUPS'
-for (i in 1:length(sol)){
-  solutions <- as.data.frame(contest[inds, which(sol$solution == i]))
-}
-
-### BUILD YOUR POSITION MATRIX FASTER
-for (j in 1:7){
-  if (j == 1 || j == 2 || j == 3){
-    
-  }
-  else (j > 3){
-    
-    
-  }
-  j <- j + 1
-  }
-  
-}
-
-#Write csv file of the optimal lineup
-write.table(solution1, 'mydata.txt', sep='\t')
